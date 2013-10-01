085196ae81bf3313ea583c780398c28ed680e80d
ChristopheDEZE <dezechristophe@gmail.com>
2013-10-01 09:29:07 +0000

    1er commit

---
+++ base.sql
@@ -0,0 +1,28 @@
+drop table if exists matchs;
+create table matchs (
+  id integer primary key autoincrement,
+  numero integer not null,
+  trou integer not null,
+  score integer not null,
+  j1 text,
+  j2 text
+);
+INSERT INTO 'matchs' VALUES(1,1,0,0,'','');
+INSERT INTO 'matchs' VALUES(2,2,0,0,'','');
+INSERT INTO 'matchs' VALUES(3,3,0,0,'','');
+INSERT INTO 'matchs' VALUES(4,4,0,0,'','');
+INSERT INTO 'matchs' VALUES(5,5,0,0,'','');
+INSERT INTO 'matchs' VALUES(6,6,0,0,'','');
+INSERT INTO 'matchs' VALUES(7,7,0,0,'','');
+INSERT INTO 'matchs' VALUES(8,8,0,0,'','');
+
+INSERT INTO 'matchs' VALUES(9,9,0,0,'','');
+INSERT INTO 'matchs' VALUES(10,10,0,0,'','');
+INSERT INTO 'matchs' VALUES(11,11,0,0,'','');
+INSERT INTO 'matchs' VALUES(12,12,0,0,'','');
+
+INSERT INTO 'matchs' VALUES(13,13,0,0,'','');
+INSERT INTO 'matchs' VALUES(14,14,0,0,'','');
+
+INSERT INTO 'matchs' VALUES(15,15,0,0,'','');
+
